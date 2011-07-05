74d8695845415dabb323cc3c0da52b5ee369ab67
Burt Beckwith <burt@burtbeckwith.com>
2011-07-05 03:08:08 -0400

    reworked the logic to ensure that the charEncodingFilter, hiddenHttpMethod, and grailsWebRequest filter mappings are first, then the security filter, then the rest

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,9 +1,15 @@
-import groovy.xml.StreamingMarkupBuilder
-
 import org.springframework.web.filter.DelegatingFilterProxy
 
 eventWebXmlEnd = { String filename ->
+	try {
+		fixWebXml()
+	}
+	catch (e) {
+		e.printStackTrace()
+	}
+}
 
+private void fixWebXml() {
 	def SpringSecurityUtils = classLoader.loadClass(
 		'org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils')
 
@@ -14,22 +20,23 @@ eventWebXmlEnd = { String filename ->
 
 	String xml = webXmlFile.text
 
-	def root = new XmlSlurper().parseText(xml)
+	def root = new XmlParser().parseText(xml)
 
-	def charEncodingFilterMapping = root.'filter-mapping'.find {
-		it.'filter-name'.text() == 'charEncodingFilter' }
+	def filterMappings = root.'filter-mapping'
 
-	// add the filter-mapping after the Spring character encoding filter
-	// or after the last filter if it's not there
-	def mappingPosition
-	if (charEncodingFilterMapping.size()) {
-		mappingPosition = charEncodingFilterMapping
-	}
-	else {
-		mappingPosition = root.filter[-1]
+	// position the security filter after the last of these,
+	// but also move anything in-between to after the security filter
+	List positions = []
+	filterMappings.eachWithIndex { fm, i ->
+		['charEncodingFilter', 'hiddenHttpMethod', 'grailsWebRequest'].each {
+			if (fm.'filter-name'.text() == it) positions << i
+		}
 	}
 
-	// the name of the filter matches the name of the Spring bean that it delegates to
+	List indexesToMove = new ArrayList((positions.min()..positions.max()))
+	indexesToMove.removeAll positions
+
+	def mappingPosition = filterMappings[positions.max()]
 	root.filter[0] + {
 		'filter' {
 			'filter-name'('springSecurityFilterChain')
@@ -44,21 +51,25 @@ eventWebXmlEnd = { String filename ->
 		}
 	}
 
+	def toMove = indexesToMove.collect { filterMappings[it] }
+
+	def filterChain = root.children().find {
+		it.name() == 'filter-mapping' &&
+		it.children()[0].text() == 'springSecurityFilterChain' }
+	int newPosition = root.children().indexOf(filterChain)
+
+	toMove.each {
+		root.children().remove(it)
+		root.children().add(newPosition, it)
+	}
+
 	webXmlFile.withWriter { it << xmlToString(root) }
 }
 
-// TODO this is not good; fix
 private String xmlToString(xml) {
-   def writer = new StringWriter()
-   writer << new StreamingMarkupBuilder().bind { mkp.yield xml }
-
-   def sw = new StringWriter()
-   def printer = new XmlNodePrinter(new PrintWriter(sw), '\t')
-   printer.preserveWhitespace = true
-   printer.print new XmlParser().parseText(writer.toString())
-
-   String rendered = sw.toString()
-   rendered = rendered.replaceAll('tag0:', '')
-   rendered = rendered.replaceAll(':tag0', '')
-   rendered
+	def writer = new StringWriter()
+	def printer = new XmlNodePrinter(new PrintWriter(writer))
+	printer.preserveWhitespace = true
+	printer.print xml
+	writer.toString()
 }
