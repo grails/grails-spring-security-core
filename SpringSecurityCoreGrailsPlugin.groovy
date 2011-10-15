1648426def74ec4d8d27b1adc2c0b19b9017129d
Burt Beckwith <burt@burtbeckwith.com>
2011-10-15 11:02:52 -0400

    1.2.2 release

--- SpringSecurityCoreGrailsPlugin.groovy
+++ SpringSecurityCoreGrailsPlugin.groovy
@@ -111,7 +111,7 @@ import org.codehaus.groovy.grails.plugins.springsecurity.WebExpressionVoter
  */
 class SpringSecurityCoreGrailsPlugin {
 
-	String version = '1.2.1'
+	String version = '1.2.2'
 	String grailsVersion = '1.2.2 > *'
 	List observe = ['controllers']
 	List loadAfter = ['controllers', 'services', 'hibernate']
