7c912cccc9324743d1b662f7614f9434dc4feca1
Burt Beckwith <beckwithb@vmware.com>
2012-01-30 18:24:22 -0500

    license update

--- scripts/S2CreatePersistentToken.groovy
+++ scripts/S2CreatePersistentToken.groovy
@@ -1,4 +1,4 @@
-/* Copyright 2006-2010 the original author or authors.
+/* Copyright 2006-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import groovy.text.SimpleTemplateEngine
 
 includeTargets << new File("$springSecurityCorePluginDir/scripts/_S2Common.groovy")
 
