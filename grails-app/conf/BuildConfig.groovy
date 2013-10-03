9fb9d3d6a1a4470635e78695c1549e17c8020048
Burt Beckwith <burt@burtbeckwith.com>
2013-10-02 23:09:05 -0400

    cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,5 @@
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for backwards-compatibility, the docs are checked into gh-pages branch
-grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
 
