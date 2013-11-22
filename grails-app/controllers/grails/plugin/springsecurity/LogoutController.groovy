8488c5fc142c46d6b3feeefb928df1ab18f0ffed
Burt Beckwith <burt@burtbeckwith.com>
2013-11-22 03:21:24 -0500

    forward instead of redirect to avoid a round trip

--- grails-app/controllers/grails/plugin/springsecurity/LogoutController.groovy
+++ grails-app/controllers/grails/plugin/springsecurity/LogoutController.groovy
@@ -32,6 +32,6 @@ class LogoutController {
 		}
 
 		// TODO put any pre-logout code here
-		redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl // '/j_spring_security_logout'
+		forward uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl // '/j_spring_security_logout'
 	}
 }
