f04dd7abde560c97f8aa5a22b0faa0885830363b
Burt Beckwith <burt@burtbeckwith.com>
2014-03-06 11:32:48 -0500

    license updates

--- src/groovy/grails/plugin/springsecurity/InterceptedUrl.groovy
+++ src/groovy/grails/plugin/springsecurity/InterceptedUrl.groovy
@@ -1,4 +1,4 @@
-/* Copyright 2013 SpringSource.
+/* Copyright 2013-2014 SpringSource.
  *
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
