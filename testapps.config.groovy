2247aa45a449d14ee9ec65171bed0c0119933600
Graeme Rocher <graeme.rocher@gmail.com>
2014-05-12 11:54:25 +0200

    attempt to run functional tests on Travis

--- testapps.config.groovy
+++ testapps.config.groovy
@@ -1,14 +1,14 @@
 String version = '2.0-RC2'
-String grailsHomeRoot = '/usr/local/javalib'
-String dotGrailsCommon = '/home/burt/.grails'
-String projectDirCommon = '/home/burt/workspace/testapps/spring-security-test'
+String grailsHomeRoot = "${System.getProperty('user.home')}/.gvm/grails"
+String dotGrailsCommon = "${System.getProperty('user.home')}/.grails"
+String projectDirCommon = 'target/testapps/spring-security-test'
 
 v20 {
 	grailsVersion = '2.0.4'
 	pluginVersion = version
 	dotGrails = dotGrailsCommon
 	projectDir = projectDirCommon
-	grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+	grailsHome = grailsHomeRoot + '/' + grailsVersion
 }
 
 v21 {
@@ -16,21 +16,21 @@ v21 {
 	pluginVersion = version
 	dotGrails = dotGrailsCommon
 	projectDir = projectDirCommon
-	grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+	grailsHome = grailsHomeRoot + '/' + grailsVersion
 }
 
 v22 {
-	grailsVersion = '2.2.4'
+	grailsVersion = '2.2.5'
 	pluginVersion = version
 	dotGrails = dotGrailsCommon
 	projectDir = projectDirCommon
-	grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+	grailsHome = grailsHomeRoot + '/' + grailsVersion
 }
 
 v23 {
-	grailsVersion = '2.3.3'
+	grailsVersion = '2.3.8'
 	pluginVersion = version
 	dotGrails = dotGrailsCommon
 	projectDir = projectDirCommon
-	grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+	grailsHome = grailsHomeRoot + '/' + grailsVersion
 }
