d8385b8e7958e0b19973be3065f356576c83db4a
Graeme Rocher <graeme.rocher@gmail.com>
2014-05-19 18:07:35 +0000

    Updating docs for Travis build: https://travis-ci.org/grails-plugins/grails-spring-security-core/builds/25537332

--- webtest/projectFiles/FooBarController.groovy
+++
@@ -1,7 +0,0 @@
-import org.springframework.security.access.annotation.Secured
-
-@Secured(['ROLE_ADMIN'])
-class FooBarController {
-	def index() { render "INDEX" }
-	def barFoo() { render "barFoo" }
-}
