c1a14594df9fd3b9e67ace591e5a3dc05ae6a404
graemerocher <graeme.rocher@gmail.com>
2014-05-19 18:17:30 +0200

    Release RC3 to support Grails 2.4

--- SpringSecurityCoreGrailsPlugin.groovy
+++ SpringSecurityCoreGrailsPlugin.groovy
@@ -129,7 +129,7 @@ import org.springframework.web.filter.DelegatingFilterProxy
  */
 class SpringSecurityCoreGrailsPlugin {
 
-	String version = '2.0-SNAPSHOT'
+	String version = '2.0-RC3'
 	String grailsVersion = '2.0.0 > *'
 	List observe = ['controllers']
 	List loadAfter = ['controllers', 'services', 'hibernate']
