6986fe2769d1ef1c5af4121cb0fe630e412e9220
graemerocher <graeme.rocher@gmail.com>
2014-05-22 00:09:55 +0200

    Make the minimum Grails version 2.3

--- SpringSecurityCoreGrailsPlugin.groovy
+++ SpringSecurityCoreGrailsPlugin.groovy
@@ -130,7 +130,7 @@ import org.springframework.web.filter.DelegatingFilterProxy
 class SpringSecurityCoreGrailsPlugin {
 
 	String version = '2.0-SNAPSHOT'
-	String grailsVersion = '2.0.0 > *'
+	String grailsVersion = '2.3.0 > *'
 	List observe = ['controllers']
 	List loadAfter = ['controllers', 'services', 'hibernate']
 
