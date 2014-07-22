63027adb3322ddd8e2e0ab8ced65b53983ec196f
graemerocher <graeme.rocher@gmail.com>
2014-07-22 10:57:28 +0200

    Back to snapshot

--- SpringSecurityCoreGrailsPlugin.groovy
+++ SpringSecurityCoreGrailsPlugin.groovy
@@ -129,7 +129,7 @@ import org.springframework.web.filter.DelegatingFilterProxy
  */
 class SpringSecurityCoreGrailsPlugin {
 
-	String version = '2.0-RC4'
+	String version = '2.0-SNAPSHOT'
 	String grailsVersion = '2.3.0 > *'
 	List observe = ['controllers']
 	List loadAfter = ['controllers', 'services', 'hibernate', 'hibernate4']
