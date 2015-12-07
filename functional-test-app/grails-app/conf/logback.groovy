5cee5ff5ef4d35e8ad02101ce3aeba1be31b1430
Burt Beckwith <burt@burtbeckwith.com>
2015-12-07 03:16:10 -0500

    updated to use a subclass of ConsoleAppender to prevent logging stacktraces from Boot's ErrorPageFilter, and from ExceptionTranslationFilter that are logged at the debug level, which clutter the output when testing

--- functional-test-app/grails-app/conf/logback.groovy
+++ functional-test-app/grails-app/conf/logback.groovy
@@ -1,18 +1,36 @@
 import grails.util.BuildSettings
 import grails.util.Environment
 
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.LoggingEvent
+import org.slf4j.LoggerFactory
+
 String defaultPattern = '%-65(%.-2level %date{HH:mm:ss.SSS} %logger{32}) - %message%n'
 
-appender('STDOUT', ConsoleAppender) {
+class ExceptionFilteringConsoleAppender extends ConsoleAppender<LoggingEvent> {
+
+	protected void writeOut(LoggingEvent event) throws IOException {
+		if (event.throwableProxy) {
+			if (event.level == DEBUG || event.loggerName == 'org.springframework.boot.context.web.ErrorPageFilter') {
+				event = new LoggingEvent(Logger.FQCN, LoggerFactory.getLogger(event.loggerName) as Logger,
+						event.level, event.message, null, event.argumentArray)
+			}
+		}
+
+		super.writeOut event
+	}
+}
+
+appender('STDOUT', ExceptionFilteringConsoleAppender) {
 	encoder(PatternLayoutEncoder) {
 		pattern = defaultPattern
 	}
 }
 
-logger 'grails.plugin.springsecurity', TRACE
-logger 'org.springframework.security', DEBUG
-logger 'org.hibernate.SQL', DEBUG
-//logger 'org.hibernate.type.descriptor.sql.BasicBinder', TRACE
+// logger 'grails.plugin.springsecurity', TRACE
+// logger 'org.springframework.security', DEBUG
+// logger 'org.hibernate.SQL', DEBUG
+// logger 'org.hibernate.type.descriptor.sql.BasicBinder', TRACE
 
 root ERROR, ['STDOUT']
 
