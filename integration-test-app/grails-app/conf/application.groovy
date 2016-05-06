dc57faf20c4eb7e2fd8faefc711f217c1e8c5e27
Burt Beckwith <burt@burtbeckwith.com>
2016-05-06 06:48:31 -0400

    merged application.yml into applicataion.groovy

--- integration-test-app/grails-app/conf/application.groovy
+++ integration-test-app/grails-app/conf/application.groovy
@@ -3,6 +3,16 @@ import org.springframework.security.authentication.CredentialsExpiredException
 import org.springframework.security.authentication.DisabledException
 import org.springframework.security.authentication.LockedException
 
+dataSource {
+	dbCreate = 'update'
+	driverClassName = 'org.h2.Driver'
+	jmxExport = false
+	password = ''
+	pooled = true
+	url = 'jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE'
+	username = 'sa'
+}
+
 grails {
 	plugin {
 		springsecurity {
@@ -49,6 +59,26 @@ grails {
 			logout {
 				additionalHandlerNames = ['additionalLogoutHandler']
 			}
- 		}
+		}
 	}
 }
+
+hibernate {
+	cache {
+		queries = false
+		use_query_cache = false
+		use_second_level_cache = false
+	}
+	format_sql = true
+	use_sql_comments = true
+}
+
+info {
+	app {
+		name = '@info.app.name@'
+		version = '@info.app.version@'
+		grailsVersion = '@info.app.grailsVersion@'
+	}
+}
+
+spring.groovy.template.'check-template-location' = false
