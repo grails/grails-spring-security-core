b7add879e01ec81a372519ed4e544da6e21b4145
Burt Beckwith <burt@burtbeckwith.com>
2016-05-06 16:57:52 -0400

    removed hyphenated url workaround since the fix has been in Grails since 3.0.10

--- functional-test-app/grails-app/conf/spring/resources.groovy
+++ functional-test-app/grails-app/conf/spring/resources.groovy
@@ -1,18 +1,12 @@
 import grails.plugin.springsecurity.SpringSecurityUtils
-import grails.web.UrlConverter
-import test.HackUrlConverter
 import test.TestRequestmapFilterInvocationDefinition
 
 beans = {
-	"$UrlConverter.BEAN_NAME"(HackUrlConverter)
-
-	String securityConfigType = SpringSecurityUtils.securityConfigType
-	def conf = SpringSecurityUtils.securityConfig
-
-	if (securityConfigType == 'Requestmap') {
+	if (SpringSecurityUtils.securityConfigType == 'Requestmap') {
 		objectDefinitionSource(TestRequestmapFilterInvocationDefinition) {
-			if (conf.rejectIfNoRule instanceof Boolean) {
-				rejectIfNoRule = conf.rejectIfNoRule
+			def reject = SpringSecurityUtils.securityConfig.rejectIfNoRule
+			if (reject instanceof Boolean) {
+				rejectIfNoRule = reject
 			}
 		}
 	}
