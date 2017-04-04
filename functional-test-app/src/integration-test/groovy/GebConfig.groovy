8bb66431cbf990ec7942a193d08fe266f170e23a
sdelamo <sergio.delamo@softamo.com>
2017-04-04 07:04:55 +0200

    configure htmlUnit and latest phantomJs

--- functional-test-app/src/integration-test/groovy/GebConfig.groovy
+++ functional-test-app/src/integration-test/groovy/GebConfig.groovy
@@ -1,48 +1,31 @@
-// See: http://www.gebish.org/manual/current/configuration.html
-
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.phantomjs.PhantomJSDriver
 import org.openqa.selenium.remote.DesiredCapabilities
 
-import geb.report.ReportState
-import geb.report.Reporter
-import geb.report.ReportingListener
-
-//System.setProperty 'webdriver.chrome.driver', 'c:/dev/chromedriver.exe'
-//System.setProperty 'phantomjs.binary.path', ''
-
-//driver = { new ChromeDriver() }
-driver = { new PhantomJSDriver(new DesiredCapabilities()) }
-
+// See: http://www.gebish.org/manual/current/configuration.html
 baseUrl = 'http://localhost:8238/'
 quitCachedDriverOnShutdown = true
 
 environments {
-
-	// run as 'grails -Dgeb.env=phantomjs test-app'
 	// See: http://code.google.com/p/selenium/wiki/HtmlUnitDriver
-	htmlunit {
-		driver = { new PhantomJSDriver(new DesiredCapabilities()) }
+	// run as 'grails -Dgeb.env=htmlUnit test-app'
+	htmlUnit {
+		driver = { new HtmlUnitDriver() }
 	}
-
-	// run as 'grails -Dgeb.env=chrome test-app'
+	// run as 'grails -Dgeb.env=chrome -Dwebdriver.chrome.driver=/Users/sdelamo/Applications/chromedriver test-app'
 	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
 	chrome {
 		driver = { new ChromeDriver() }
 	}
-
 	// run as 'grails -Dgeb.env=firefox test-app'
 	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
 	firefox {
 		driver = { new FirefoxDriver() }
 	}
+	// run as 'grails -Dgeb.env=phantomJs -Dphantomjs.binary.path=/Users/sdelamo/Applications/phantomjs-2.1.1-macosx/bin/phantomjs test-app'
+	phantomJs {
+		driver = { new PhantomJSDriver(new DesiredCapabilities()) }
+	}
 }
-
-//reportingListener = new ReportingListener() {
-//	void onReport(Reporter reporter, ReportState reportState, List<File> reportFiles) {
-//		reportFiles.each {
-//			println "Report taken: $it.absolutePath"
-//		}
-//	}
-//}
