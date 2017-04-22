cc300a65a37b1b26d8467d1284dabaef9af37296
sdelamo <sergio.delamo@softamo.com>
2017-04-22 08:20:41 +0200

    Add script to execute misc functional tests

--- run-misc-functional-tests.sh
+++ run-misc-functional-tests.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
 
 PROJECTS="grails-spring-security-group grails-spring-security-hierarchical-roles"
-GORM_VERSIONS="6.0.9.RELEASE 6.1.1.RELEASE"
+GORM_VERSIONS="6.1.1.RELEASE 6.0.9.RELEASE"
 
 set -e
 
 for project in $PROJECTS; do
+    
+    echo "cd misc-functional-test-app/$project" 
+    cd misc-functional-test-app/$project
 
     for gormVersion in $GORM_VERSIONS; do
+       
+        rm -rf build
 
         echo "running tests for project: $project with GORM version: $gormVersion"
 
-        cd misc-functional-test-app/$project 
-        
-        rm -rf build
+        echo "./gradlew -PnewGormVersion=$gormVersion clean check --stacktrace"
 
-        rm -rf .gradle
-
-        ./gradlew deleteS2QuickstartGeneratedFiles
-
-        ./gradlew -PnewGormVersion=$gormVersion updateGormVersion
-
-        ./gradlew check
-
-        cd ../..
+        ./gradlew -PnewGormVersion=$gormVersion clean check --stacktrace
 
     done
+
+    echo "cd ../.."
+    cd ../..
 
 done
