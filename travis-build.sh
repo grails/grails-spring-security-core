e519536884b87bb49cc884875d83459252b08604
sdelamo <sergio.delamo@softamo.com>
2017-04-22 11:49:13 +0200

    publish docs

--- travis-build.sh
+++ travis-build.sh
@@ -24,31 +24,7 @@ if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
 	./gradlew bintrayUpload --stacktrace
 
-	./gradlew docsMobi --stacktrace
-
-	git config --global user.name "$GIT_NAME"
-	git config --global user.email "$GIT_EMAIL"
-	git config --global credential.helper "store --file=~/.git-credentials"
-	echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
-
-	git checkout gh-pages
-
-	git rm v3/spring-security-core-*.epub
-	mv build/docs/spring-security-core-*.epub v3
-	git add v3/spring-security-core-*.epub
-
-	git rm v3/spring-security-core-*.pdf
-	mv build/docs/spring-security-core-*.pdf v3
-	git add v3/spring-security-core-*.pdf
-
-	mv build/docs/index.html v3
-	git add v3/index.html
-
-	mv build/docs/ghpages.html index.html
-	git add index.html
-
-	git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-	git push origin
+    publish-docs.sh
 
 	fi
 
