4bc77097acd8986e09389efcf059b630220dedfa
sdelamo <sergio.delamo@softamo.com>
2017-04-24 11:22:02 +0200

    call run-all-tests.sh script

--- travis-build.sh
+++ travis-build.sh
@@ -3,16 +3,9 @@
 set -e
 
 echo "travis tag: $TRAVIS_TAG"
-
 rm -rf build
 
-./gradlew -q clean check install --stacktrace
-
-integration-test-app/run_integration_tests.sh
-
-./copy_functional_tests_to_different_grails_versions.sh
-
-./run_functional_tests.sh
+./run-all-tests.sh
 
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
