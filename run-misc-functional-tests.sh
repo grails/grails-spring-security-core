5e2a1f3180d6992fd4889b633bfdb8f02be48674
sdelamo <sergio.delamo@softamo.com>
2017-05-16 13:28:03 +0200

    Run misc test

--- run-misc-functional-tests.sh
+++ run-misc-functional-tests.sh
@@ -1,18 +1,41 @@
 #!/usr/bin/env bash
 
+set -e
+
+curl -s http://get.sdkman.io | bash
+echo sdkman_auto_answer=true > ~/.sdkman/etc/config
+echo "source \"$HOME/.sdkman/bin/sdkman-init.sh\""
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
 PROJECTS="grails-spring-security-group grails-spring-security-hierarchical-roles"
 GORM_VERSIONS="6.1.1.RELEASE 6.0.9.RELEASE"
 
-set -e
+
+GRADLE_PROPERTIES_FILE=gradle.properties
+
+function getProperty {
+   PROP_KEY=$1
+   PROP_VALUE=`cat $GRADLE_PROPERTIES_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
+   echo $PROP_VALUE
+}
+
 
 for project in $PROJECTS; do
-    
-    echo "cd misc-functional-test-app/$project" 
+
+    echo "cd misc-functional-test-app/$project"
     cd misc-functional-test-app/$project
 
     rm -rf build
 
     rm -rf .gradle
+
+    GRAILS_VERSION=$(getProperty "grailsVersion")
+
+    sdk install grails $GRAILS_VERSION
+
+    echo "sdk use grails $GRAILS_VERSION"
+
+    sdk use grails $GRAILS_VERSION
 
     echo "running tests for project: $project"
 
