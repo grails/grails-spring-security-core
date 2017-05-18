98b01e9e6054c2caeb14cfdedb4e52654084be43
sdelamo <sergio.delamo@softamo.com>
2017-05-18 10:17:19 +0200

    Revert "Add v3.3 docs"
    This reverts commit 2aa95ba32d5fc261a7162879cf158450ced8b7dc.

--- publish-docs.sh
+++ publish-docs.sh
@@ -11,22 +11,23 @@ git config --global user.email "$GIT_EMAIL"
 git config --global credential.helper "store --file=~/.git-credentials"
 echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
 
+
 echo "Publishing Documentation"
 
 git clone https://${GH_TOKEN}@github.com/grails-plugins/grails-spring-security-core.git -b gh-pages gh-pages --single-branch > /dev/null
 
 cd gh-pages
 
-git rm v3.3/spring-security-core-*.epub
+git rm v3/spring-security-core-*.epub
 mv  ../build/docs/spring-security-core-*.epub v3
-git add v3.3/spring-security-core-*.epub
+git add v3/spring-security-core-*.epub
 
-git rm v3.3/spring-security-core-*.pdf
+git rm v3/spring-security-core-*.pdf
 mv ../build/docs/spring-security-core-*.pdf v3
-git add v3.3/spring-security-core-*.pdf
+git add v3/spring-security-core-*.pdf
 
 mv ../build/docs/index.html v3
-git add v3.3/index.html
+git add v3/index.html
 
 mv ../build/docs/ghpages.html index.html
 git add index.html
