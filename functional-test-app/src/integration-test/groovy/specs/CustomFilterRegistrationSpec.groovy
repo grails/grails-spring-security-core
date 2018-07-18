cf4db83d8a2deb98e8c0d4b8c063f5509518b203
sdelamo <sergio.delamo@softamo.com>
2018-07-18 13:11:01 +0200

    Unnecessary serverPort inject

--- functional-test-app/src/integration-test/groovy/specs/CustomFilterRegistrationSpec.groovy
+++ functional-test-app/src/integration-test/groovy/specs/CustomFilterRegistrationSpec.groovy
@@ -2,7 +2,6 @@ package specs
 
 import grails.plugins.rest.client.RestBuilder
 import grails.plugins.rest.client.RestResponse
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import spock.lang.IgnoreIf
 import spock.lang.Issue
@@ -10,9 +9,6 @@ import spock.lang.Issue
 @IgnoreIf({ System.getProperty('TESTCONFIG') != 'issue503' })
 @Issue('https://github.com/grails-plugins/grails-spring-security-core/issues/503')
 class CustomFilterRegistrationSpec extends AbstractSecuritySpec {
-
-    @Value('${local.server.port}')
-    Integer serverPort
 
     void 'GET request to /assets/spinner.gif should not throw error because custom filter is excluded'() {
         given: "MaintenanceModeFilter is registered in BootStrap and we have a restBuilder"
