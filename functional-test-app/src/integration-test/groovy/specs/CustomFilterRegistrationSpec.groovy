0f311ad9c56e91067d7928809ffcd747de5a5649
sdelamo <sergio.delamo@softamo.com>
2018-12-13 09:21:53 +0100

    replace RestBuilder with Micronaut HttpClient
    # Conflicts:
    #	gradle.properties

--- functional-test-app/src/integration-test/groovy/specs/CustomFilterRegistrationSpec.groovy
+++ functional-test-app/src/integration-test/groovy/specs/CustomFilterRegistrationSpec.groovy
@@ -1,23 +1,22 @@
 package specs
 
-import grails.plugins.rest.client.RestBuilder
-import grails.plugins.rest.client.RestResponse
-import org.springframework.http.HttpStatus
+import grails.testing.mixin.integration.Integration
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import spock.lang.IgnoreIf
 import spock.lang.Issue
 
 @IgnoreIf({ System.getProperty('TESTCONFIG') != 'issue503' })
 @Issue('https://github.com/grails-plugins/grails-spring-security-core/issues/503')
-class CustomFilterRegistrationSpec extends AbstractSecuritySpec {
+@Integration
+class CustomFilterRegistrationSpec extends HttpClientSpec {
 
     void 'GET request to /assets/spinner.gif should not throw error because custom filter is excluded'() {
-        given: "MaintenanceModeFilter is registered in BootStrap and we have a restBuilder"
-        RestBuilder restBuilder = new RestBuilder()
-
         when: "A GET request to the assets directory is made"
-        RestResponse response = restBuilder.get("http://localhost:${serverPort}/assets/spinner.gif")
+        HttpResponse response = client.toBlocking().exchange(HttpRequest.GET("/assets/spinner.gif"))
 
         then: "the filter is not invoked because of the chainMap defition of filters: 'none' in application.groovy"
-        response.status == HttpStatus.OK.value()
+        response.status == HttpStatus.OK
     }
 }
--- functional-test-app/src/integration-test/groovy/specs/TestFormParamsControllerSpec.groovy
+++ functional-test-app/src/integration-test/groovy/specs/TestFormParamsControllerSpec.groovy
@@ -1,13 +1,13 @@
 package specs
 
-import grails.plugins.rest.client.RestBuilder
-import grails.plugins.rest.client.RestResponse
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpStatus
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
+import grails.testing.mixin.integration.Integration
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.exceptions.HttpClientResponseException
+import io.micronaut.http.uri.UriTemplate
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
-import org.springframework.web.client.RestTemplate
 import spock.lang.IgnoreIf
 import spock.lang.Issue
 
@@ -17,209 +17,147 @@ import spock.lang.Issue
  */
 @IgnoreIf({ System.getProperty('TESTCONFIG') != 'putWithParams' })
 @Issue('https://github.com/grails-plugins/grails-spring-security-core/issues/554')
-class TestFormParamsControllerSpec extends AbstractSecuritySpec {
+@Integration
+class TestFormParamsControllerSpec extends HttpClientSpec {
 
     private final String USERNAME = "Admin"
     private final String PASSWORD = "myPassword"
 
     void 'PUT request with no parameters'() {
-        given:
-        RestBuilder restBuilder = new RestBuilder()
-
         when: "A PUT request with no parameters is made"
-        RestResponse response = restBuilder.put("http://localhost:${serverPort}/testFormParams/permitAll") {
-            contentType("application/x-www-form-urlencoded")
-        }
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.PUT("/testFormParams/permitAll", "").contentType("application/x-www-form-urlencoded"), String)
 
         then: "the controller responds with the correct status and parameters are null"
-        response.status == HttpStatus.OK.value()
-        response.text == "username: null, password: null"
+        response.status == HttpStatus.OK
+        response.body() == "username: null, password: null"
     }
 
     void 'PUT request with parameters in the URL'() {
-        given:
-        RestBuilder restBuilder = new RestBuilder()
-
         when: "A PUT request with no parameters is made"
-        RestResponse response = restBuilder.put("http://localhost:${serverPort}/testFormParams/permitAll?username={username}&password={password}") {
-            urlVariables([username: USERNAME, password: PASSWORD])
-            contentType("application/x-www-form-urlencoded")
-        }
+        String expandUrl = new UriTemplate("/testFormParams/permitAll{?username,password}").expand(["username": USERNAME, "password": PASSWORD])
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.PUT(expandUrl, "").contentType("application/x-www-form-urlencoded"), String)
 
         then: "the controller responds with the correct status and parameters are extracted"
-        response.status == HttpStatus.OK.value()
-        response.text == "username: ${USERNAME}, password: ${PASSWORD}"
+        response.status == HttpStatus.OK
+        response.body() == "username: ${USERNAME}, password: ${PASSWORD}"
     }
 
     void 'PUT request with parameters as x-www-form-urlencoded'() {
-        given: "a RestBuilder"
-        RestBuilder restBuilder = new RestBuilder()
-
-        and: "a form with username and password params"
+        given: "a form with username and password params"
         MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>()
         form.add("username", USERNAME)
         form.add("password", PASSWORD)
 
         when: "A PUT request with form params is made"
-        RestResponse response = restBuilder.put("http://localhost:${serverPort}/testFormParams/permitAll") {
-            contentType("application/x-www-form-urlencoded")
-            body(form)
-        }
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.PUT("/testFormParams/permitAll", form).contentType("application/x-www-form-urlencoded"), String)
 
         then: "the controller responds with the correct status and parameters are extracted"
-        response.status == HttpStatus.OK.value()
-        response.text == "username: ${USERNAME}, password: ${PASSWORD}"
+        response.status == HttpStatus.OK
+        response.body() == "username: ${USERNAME}, password: ${PASSWORD}"
     }
 
     void 'PUT request with NULL Content-Type and parameters in the URL'() {
-        given:
-        RestBuilder restBuilder = new RestBuilder()
-
         when: "A PUT request with no parameters is made"
-        RestResponse response = restBuilder.put("http://localhost:${serverPort}/testFormParams/permitAll?username={username}&password={password}") {
-            urlVariables([username: USERNAME, password: PASSWORD])
-        }
+        String expandUrl = new UriTemplate("/testFormParams/permitAll{?username,password}").expand(["username": USERNAME, "password": PASSWORD])
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.PUT(expandUrl, ""), String)
 
         then: "the controller responds with the correct status and parameters are extracted"
-        response.status == HttpStatus.OK.value()
-        response.text == "username: ${USERNAME}, password: ${PASSWORD}"
+        response.status == HttpStatus.OK
+        response.body() == "username: ${USERNAME}, password: ${PASSWORD}"
     }
 
     void 'PUT request with NULL Content-Type'() {
-        given:
-        RestBuilder restBuilder = new RestBuilder()
-
         when: "A PUT request with NULL Content-Type is made"
-        RestResponse response = restBuilder.put("http://localhost:${serverPort}/testFormParams/permitAll")
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.PUT("/testFormParams/permitAll", ""), String)
 
         then: "the controller responds with the correct status and parameters are null"
-        response.status == HttpStatus.OK.value()
-        response.text == "username: null, password: null"
+        response.status == HttpStatus.OK
+        response.body() == "username: null, password: null"
     }
 
     void 'PATCH request with no parameters'() {
-        given: "An HTTP client that supports PATCH requests"
-        RestTemplate restTemplate = new RestTemplate()
-        restTemplate.requestFactory = new HttpComponentsClientHttpRequestFactory()
-        RestBuilder restBuilder = new RestBuilder(restTemplate)
-
         when: "A PATCH request with no parameters is made"
-        RestResponse response = restBuilder.patch("http://localhost:${serverPort}/testFormParams/permitAll") {
-            contentType("application/x-www-form-urlencoded")
-        }
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.PATCH("/testFormParams/permitAll", "").contentType("application/x-www-form-urlencoded"), String)
 
         then: "the controller responds with the correct status and parameters are null"
-        response.status == HttpStatus.OK.value()
-        response.text == "username: null, password: null"
+        response.status == HttpStatus.OK
+        response.body() == "username: null, password: null"
     }
 
     void 'PATCH request with parameters in the URL'() {
-        given: "An HTTP client that supports PATCH requests"
-        RestTemplate restTemplate = new RestTemplate()
-        restTemplate.requestFactory = new HttpComponentsClientHttpRequestFactory()
-        RestBuilder restBuilder = new RestBuilder(restTemplate)
-
-        when: "A PATCH request with no parameters is made"
-        RestResponse response = restBuilder.patch("http://localhost:${serverPort}/testFormParams/permitAll?username={username}&password={password}") {
-            urlVariables([username: USERNAME, password: PASSWORD])
-            contentType("application/x-www-form-urlencoded")
-        }
+        when:
+        String expandUrl = new UriTemplate("/testFormParams/permitAll{?username,password}").expand(["username": USERNAME, "password": PASSWORD])
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.PATCH(expandUrl, "").contentType("application/x-www-form-urlencoded"), String)
 
         then: "the controller responds with the correct status and parameters are extracted"
-        response.status == HttpStatus.OK.value()
-        response.text == "username: ${USERNAME}, password: ${PASSWORD}"
+        response.status == HttpStatus.OK
+        response.body() == "username: ${USERNAME}, password: ${PASSWORD}"
     }
 
     void 'PATCH request with parameters as x-www-form-urlencoded'() {
-        given: "An HTTP client that supports PATCH requests"
-        RestTemplate restTemplate = new RestTemplate()
-        restTemplate.requestFactory = new HttpComponentsClientHttpRequestFactory()
-        RestBuilder restBuilder = new RestBuilder(restTemplate)
-
-        and: "a form with username and password params"
+        given: "a form with username and password params"
         MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>()
         form.add("username", USERNAME)
         form.add("password", PASSWORD)
 
         when: "A PATCH request with form params is made"
-        RestResponse response = restBuilder.patch("http://localhost:${serverPort}/testFormParams/permitAll") {
-            contentType("application/x-www-form-urlencoded")
-            body(form)
-        }
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.PATCH("/testFormParams/permitAll", form
+        ).contentType("application/x-www-form-urlencoded"), String)
 
         then: "the controller responds with the correct status and parameters are extracted"
-        response.status == HttpStatus.OK.value()
-        response.text == "username: ${USERNAME}, password: ${PASSWORD}"
+        response.status == HttpStatus.OK
+        response.body() == "username: ${USERNAME}, password: ${PASSWORD}"
     }
 
     void 'PUT request to secured endpoint with parameters as x-www-form-urlencoded'() {
-        given: "a RestBuilder"
-        RestBuilder restBuilder = new RestBuilder()
-
-        and: "a form with username and password params"
+        given: "a form with username and password params"
         MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>()
         form.add("username", USERNAME)
         form.add("password", PASSWORD)
 
         when: "A PUT request with form params is made to a secured endpoint"
-        RestResponse response = restBuilder.put("http://localhost:${serverPort}/testFormParams/permitAdmin") {
-            contentType("application/x-www-form-urlencoded")
-            body(form)
-        }
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.PUT("/testFormParams/permitAdmin", form
+        ).contentType("application/x-www-form-urlencoded"), String)
 
         then: "the request is not processed by the controller"
-        response.status == HttpStatus.INTERNAL_SERVER_ERROR.value()
+        HttpClientResponseException e = thrown()
+        e.status == HttpStatus.INTERNAL_SERVER_ERROR
     }
 
     void 'PATCH request to secured endpoint with parameters as x-www-form-urlencoded'() {
-        given: "An HTTP client that supports PATCH requests"
-        RestTemplate restTemplate = new RestTemplate()
-        restTemplate.requestFactory = new HttpComponentsClientHttpRequestFactory()
-        RestBuilder restBuilder = new RestBuilder(restTemplate)
-
-        and: "a form with username and password params"
+        given:
         MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>()
         form.add("username", USERNAME)
         form.add("password", PASSWORD)
 
         when: "A PATCH request with form params is made to a secured endpoint"
-        RestResponse response = restBuilder.patch("http://localhost:${serverPort}/testFormParams/permitAdmin") {
-            contentType("application/x-www-form-urlencoded")
-            body(form)
-        }
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.PATCH("/testFormParams/permitAdmin", form
+        ).contentType("application/x-www-form-urlencoded"), String)
 
         then: "the request is not processed by the controller"
-        response.status == HttpStatus.INTERNAL_SERVER_ERROR.value()
+        HttpClientResponseException e = thrown()
+        e.status == HttpStatus.INTERNAL_SERVER_ERROR
     }
 
     void 'PATCH request with NULL Content-Type and parameters in the URL'() {
-        given: "An HTTP client that supports PATCH requests"
-        RestTemplate restTemplate = new RestTemplate()
-        restTemplate.requestFactory = new HttpComponentsClientHttpRequestFactory()
-        RestBuilder restBuilder = new RestBuilder(restTemplate)
-
-        when: "A PUT request with no parameters is made"
-        RestResponse response = restBuilder.patch("http://localhost:${serverPort}/testFormParams/permitAll?username={username}&password={password}") {
-            urlVariables([username: USERNAME, password: PASSWORD])
-        }
+        when:
+        String expandUrl = new UriTemplate("/testFormParams/permitAll{?username,password}").expand(["username": USERNAME, "password": PASSWORD])
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.PATCH(expandUrl, ""
+        ).contentType("application/x-www-form-urlencoded"), String)
 
         then: "the controller responds with the correct status and parameters are extracted"
-        response.status == HttpStatus.OK.value()
-        response.text == "username: ${USERNAME}, password: ${PASSWORD}"
+        response.status == HttpStatus.OK
+        response.body() == "username: ${USERNAME}, password: ${PASSWORD}"
     }
 
     void 'PATCH request with NULL Content-Type'() {
-        given: "An HTTP client that supports PATCH requests"
-        RestTemplate restTemplate = new RestTemplate()
-        restTemplate.requestFactory = new HttpComponentsClientHttpRequestFactory()
-        RestBuilder restBuilder = new RestBuilder(restTemplate)
-
         when: "A PATCH request with NULL Content-Type is made"
-        RestResponse response = restBuilder.patch("http://localhost:${serverPort}/testFormParams/permitAll")
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.PATCH("/testFormParams/permitAll", ""
+        ), String)
 
         then: "the controller responds with the correct status and parameters are null"
-        response.status == HttpStatus.OK.value()
-        response.text == "username: null, password: null"
+        response.status == HttpStatus.OK
+        response.body() == "username: null, password: null"
     }
 
 }
