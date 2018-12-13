7bf1ff8503e9f88d210e2e655e27f0f7391f26f3
sdelamo <sergio.delamo@softamo.com>
2018-12-13 10:18:09 +0100

    remove non property access to @EqualsAndHashCode
    27: Error during @EqualsAndHashCode processing: 'includes' property 'access' does not exist.
     @ line 27, column 1.
       @EqualsAndHashCode(includes='pattern,access,httpMethod')
       ^

--- plugin/src/main/groovy/grails/plugin/springsecurity/InterceptedUrl.groovy
+++ plugin/src/main/groovy/grails/plugin/springsecurity/InterceptedUrl.groovy
@@ -24,7 +24,7 @@ import org.springframework.security.access.ConfigAttribute
 /**
  * @author Burt Beckwith
  */
-@EqualsAndHashCode(includes='pattern,access,httpMethod')
+@EqualsAndHashCode(includes='pattern,httpMethod')
 @ToString(includeNames=true)
 @CompileStatic
 class InterceptedUrl {
